4734a21cf6e9646ed2cabc24f1317536b9ff86c0
farhan11166 <farhan.alam3751@gmail.com>
2026-05-30 15:52:45 +0530

    feat: implement screen clearing functionality in kernel startup sequence

--- src/kernel.c
+++ src/kernel.c
@@ -9,7 +9,16 @@ void print(const char* msg,int row){
 
     }
 }
+void clear_screen() {
+    char *video_memory = (char*) 0xb8000;
+
+    for (int i = 0; i < 80 * 25; i++) {
+        video_memory[i * 2] = ' ';
+        video_memory[i * 2 + 1] = 0x00;
+    }
+}
 void kernel_main(){
+    clear_screen();
     print("Welcome to CIndy-os",1);
     print("Boot Succesful",0);
 
